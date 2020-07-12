3d216a1d8d0fcfff5fd24bb62b2ebd262dcb8d24
yaseenmd <connect2yaseen@gmail.com>
2020-07-12 20:42:46 +0530

    first thread creation

--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -2,6 +2,17 @@ public class Main {
 
     public static void main(String args[])
     {
-        
+             Runnable runnable = new Runnable() {
+                 @Override
+                 public void run() {
+                     String name = Thread.currentThread().getName();
+                     System.out.println("Thread Name :" +name);
+                 }
+             };
+
+             Thread thread = new Thread(runnable);
+             thread.start();
+
+             //Output: Thread Name :Thread-0
     }
 }
